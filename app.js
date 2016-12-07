1f2697753ec79475de7ff347869e069fc4fe152e
KimJangHee <dreamquestmaster@gmail.com>
2016-12-08 02:45:30 +0900

    3 commit

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.get('/',function (req,res){
-  res.send('하하하....!!!');
-});
+app. use(express.static(path.join(__dirname + '/public')));
 
 app.listen(3000, function(){
   console.log('Sever On!');
